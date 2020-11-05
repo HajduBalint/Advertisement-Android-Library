3ba708a0de77887aff4ca4add3e593c91fdd1ce0
Hajdu Bálint <47858516+XelnoX@users.noreply.github.com>
2020-11-05 13:16:45 +0100

    refactoring filtering

--- AdvertiserLibrary/src/main/java/hu/advertisersystem/advertiserlibrary/retrofit/RetrofitClient.kt
+++ AdvertiserLibrary/src/main/java/hu/advertisersystem/advertiserlibrary/retrofit/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://localhost:8080"
+    private const val BASE_URL = "https://advertisement-system-291705.ey.r.appspot.com"
     var advertisementService: RetroFitAdvertisement? = null
 
     fun createAdvertisementService(){
